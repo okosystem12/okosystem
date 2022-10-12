7bdf779246239dd26cf8bf3904f81f00e7f8475b
Alexander Yanin <oko@oko.oko>
2022-10-12 15:23:37 +0300

    изменения на 12.10.2022 год
    1) выкачивание фото
    2) полная обработка текста страницы
    3) gitignore

--- genering_profils_vk/src/func.py
+++ genering_profils_vk/src/func.py
@@ -24,15 +24,30 @@ except ImportError:
     from urllib.request import urlopen as urlopen
     from urllib.parse import urlencode as urlencode
 
+list_data = []
 
 # глобальные переменные ддя словаря (общее кол-во фото и путь до директории с фото)
-count_photo_global = -1
+count_photo_global = 0
 path_dir_photos_global = ""
 
 # глобальные переменные ддя словаря (общее кол-во ПОСТОВ и путь до файла с найденными постами)
-count_text_global = -1
+count_text_global = 0
 path_file_text_global = ""
 
+# глобальные переменные ддя словаря (общее кол-во ГРУПП и путь до файла с найденными названиями групп)
+count_text_groups_global = 0
+path_file_text_groups_global = ""
+
+# глобальные переменные ддя словаря (общее кол-во ВИДЕО и путь до файла с найденными названиями ВИДЕО)
+count_video_global = 0
+path_file_video_global = ""
+
+# глобальные переменные для словаря (общее кол-во НАЙДЕННЫХ ЗАПИСЕЙ В СЛОВАРЕ и путь до файла с найденными ЗАПИСЯМИ)
+count_key_data_global = 0
+path_file_inf_users_global = ""
+
+
+# ====================================================================СОЗДАНИЕ СЛОВАРЕЙ ДЛЯ БАЗЫ========================
 # создание словаря для базы (ФОТО)
 def create_dict_data(url, check_date, count_record, path_dir_photos, category, data):
     data[url] ={"check_date": check_date,
@@ -63,6 +78,51 @@ def create_dict_data_post(url_user, check_date, count_record, path_post, categor
                           }
 
 
+# создание словаря для базы (НАЗВАНИЯ ГРУПП)
+def create_dict_data_name_grops(url_user, check_date, count_record, path_groups, category, database):
+    database[url_user] = {"check_date": check_date,
+                          "inf_profile": {
+                              "materials_profiles": {
+                                  "count_record": count_record,
+                                  "check_record": {
+                                      "path_groups": path_groups,
+                                      "category": category,
+                                  }
+                              }
+                          }
+                          }
+
+
+# создание словаря для базы (НАЗВАНИЯ ВИДЕО)
+def create_dict_data_name_videos(url_user, check_date, count_record, path_videos, category, database):
+    database[url_user] = {"check_date": check_date,
+                          "inf_profile": {
+                              "materials_profiles": {
+                                  "count_record": count_record,
+                                  "check_record": {
+                                      "path_videos": path_videos,
+                                      "category": category,
+                                  }
+                              }
+                          }
+                          }
+
+
+# создание словаря для базы (ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ)
+def create_dict_data_inf_users(url_user, check_date, count_key_data, path_inf_user, category, database):
+    database[url_user] = {"check_date": check_date,
+                          "inf_profile": {
+                              "materials_profiles": {
+                                  "count_record": count_key_data,
+                                  "check_record": {
+                                      "path_videos": path_inf_user,
+                                      "category": category,
+                                  }
+                              }
+                          }
+                          }
+
+
 # создание каталога с датой и временем проверки
 def create_dir_current_date_and_time(datetime):
     newpath = os.path.abspath(os.path.join("..", "verification_result", datetime))
@@ -71,12 +131,9 @@ def create_dir_current_date_and_time(datetime):
     return newpath
 
 
-# поиск по клбчевым словам среди постов
+# ===========================================================================ПОИСК ПО КРИТЕРИЯМ=========================
+# поиск по ключевым словам среди постов + {дата: имя}
 def search_post_vk_id(owner_id, abs_dir_path_user, lst_dict=config.lst_dict, token=config.token):
-    # lst_dict = ["вконтакте", "vk", "вк"]
-    # token = "vk1.a.kjUvlFSmGlEkbjDz0YK-Si0Zs_ZirSzUryCpiABbACKPVRjtAzQLuMU92wDk8c-vMA43QJt1Dqf-a0XZpNmdX5W82kCek2obC0nMdAGMiFrUrvSk4LxvujyzEFx_ytcr8RgchiMXNpBMYcsIvLO-90TsbL1kQKLXh3QoALCxP-k1_VpV8iSxV84SHW8I4Kz3"
-    #
-    # owner_id = 1
     url = "https://api.vk.com/method/execute?"
     api = 'API.wall.get({"owner_id":"' + str(owner_id) + '", "count":"1"})'
     code = f'return [{api}];'
@@ -102,9 +159,9 @@ def search_post_vk_id(owner_id, abs_dir_path_user, lst_dict=config.lst_dict, tok
             for i_resp in range(len((resp["response"][0]["items"]))):
                 for word_dict in lst_dict:
                     if word_dict.lower() in resp["response"][0]["items"][i_resp]["text"].lower():
-                        print(resp["response"][0]["items"][i_resp]["text"])
                         with open(os.path.join(abs_dir_path_user, f"post_user_{owner_id}.txt"), "a", encoding="utf-8") as file:
-                            file.write(resp["response"][0]["items"][i_resp]["text"] + "\n" + "=" * 1000 + "\n")
+                            dt = (resp["response"][0]["items"][i_resp]["date"])
+                            file.write(datetime.utcfromtimestamp(dt).strftime('%Y-%m-%d %H:%M:%S') + ": " + resp["response"][0]["items"][i_resp]["text"] + "\n" + "=" * 1000 + "\n")
                         break
         except Exception:
             break
@@ -195,8 +252,6 @@ def downloading_search_photos(user_id, dir_path_date_and_time, token=config.toke
                     for each in photos_response:
                         link = extract_pirture_url(each)
                         f.write('%s:%s\n' % (str(uid), link))
-                        # print('collecting %s:%s' % (str(uid), link))
-                    print('collecting %s of %s' % (str(i + 1), str(fave_iterations)))
                 f.close()
             except Exception:
                 pass
@@ -225,10 +280,8 @@ def downloading_search_photos(user_id, dir_path_date_and_time, token=config.toke
                             try:
                                 link = extract_pirture_url(each)
                                 f.write('%s:%s\n' % (str(uid), link))
-                                # print('collecting %s:%s' % (str(uid), link))
                             except Exception:
                                 pass
-                    print('collecting %s of %s' % (str(i + 1), str(fave_iterations)))
                 f.close()
             except Exception:
                 pass
@@ -262,7 +315,7 @@ def downloading_search_photos(user_id, dir_path_date_and_time, token=config.toke
         os.makedirs(path_user_id)
 
     # создание файла со ссылками в этом каталоге
-    file_with_photos = os.path.abspath(os.path.join(path_user_id, '%s.txt' % str(user_id)))
+    file_with_photos = os.path.abspath(os.path.join(path_user_id, '%s.txt' % f"photos_user_{str(user_id)}"))
     create_file(file_with_photos, True)
 
     # создание каталога для фоток в каталоге с id пользователя
@@ -285,7 +338,7 @@ def downloading_search_photos(user_id, dir_path_date_and_time, token=config.toke
     for number, link in enumerate(links):
         try:
             url_as = link[link.find(':') + 1:]
-            file_name = str(number) + ".jpg"
+            file_name = str(number+1) + ".jpg"
             file_name_abs = os.path.join(path_photos, file_name)
             if not os.path.isfile(file_name_abs):
                 resource = urlopen(url_as)
@@ -307,11 +360,130 @@ def downloading_search_photos(user_id, dir_path_date_and_time, token=config.toke
         os.rmdir(path_user_id)
 
 
+# поиск по ключевым словам среди списка сообществ
+def search_name_groups_vk_id(user_id, abs_dir_path_user, lst_dict=config.lst_dict, token=config.token):
+    url = "https://api.vk.com/method/execute?"
+    api = 'API.groups.get({"user_id":"' + str(user_id) + '", "count":"1"})'
+    code = f'return [{api}];'
+    data = dict(code=code, access_token=token, v='5.131')
+    resp = requests.post(url=url, data=data)
+    resp = resp.json()
+    count_record = resp["response"][0]["count"]
+    global count_text_groups_global
+    count_text_groups_global = count_record
+    global path_file_text_groups_global
+    path_file_text_groups_global = os.path.join(abs_dir_path_user, f"groups_user_{user_id}.txt")
+
+    for i in range(count_record):
+        url = "https://api.vk.com/method/execute?"
+        api = 'API.groups.get({"user_id":"' + str(user_id) + '", "count":"1", "extended":"1", "offset":"' + str(
+            i) + '"})'
+        code = f'return [{api}];'
+        data = dict(code=code, access_token=token, v='5.131')
+        resp = requests.post(url=url, data=data)
+        resp = resp.json()
+
+        try:
+            for i_resp in range(len((resp["response"][0]["items"]))):
+                for word_dict in lst_dict:
+                    if word_dict.lower() in resp["response"][0]["items"][i_resp]["name"].lower():
+                        with open(os.path.join(abs_dir_path_user, f"groups_user_{user_id}.txt"), "a",
+                                  encoding="utf-8") as file:
+                            file.write(resp["response"][0]["items"][i_resp]["name"] + "\n" + "=" * 1000 + "\n")
+                        break
+        except Exception:
+            pass
+
+
+# поиск по ключевым словами среди списка видеозаписей + {дата: имя}
+def search_name_videos_vk_id(owner_id, abs_dir_path_user, lst_dict=config.lst_dict, token=config.token):
+    url = "https://api.vk.com/method/execute?"
+    api = 'API.video.get({"user_id":"' + str(owner_id) + '", "count":"1"})'
+    code = f'return [{api}];'
+    data = dict(code=code, access_token=token, v='5.131')
+    resp = requests.post(url=url, data=data)
+    resp = resp.json()
+    count_record = resp["response"][0]["count"]
+    global count_video_global
+    count_video_global = count_record
+    global path_file_video_global
+    path_file_video_global = os.path.join(abs_dir_path_user, f"videos_user_{owner_id}.txt")
+
+    for i in range(count_record):
+        url = "https://api.vk.com/method/execute?"
+        api = 'API.video.get({"user_id":"' + str(owner_id) + '", "count":"1", "extended":"1", "offset":"' + str(i) + '"})'
+        code = f'return [{api}];'
+        data = dict(code=code, access_token=token, v='5.131')
+        resp = requests.post(url=url, data=data)
+        resp = resp.json()
+
+        try:
+            for i_resp in range(len((resp["response"][0]["items"]))):
+                for word_dict in lst_dict:
+                    if word_dict.lower() in resp["response"][0]["items"][0]["title"].lower():
+                        with open(os.path.join(abs_dir_path_user, f"videos_user_{owner_id}.txt"), "a", encoding="utf-8") as file:
+                            dt = (resp["response"][0]["items"][0]["date"])
+                            file.write(datetime.utcfromtimestamp(dt).strftime('%Y-%m-%d %H:%M:%S') + ": " + resp["response"][0]["items"][0]["title"] + "\n" + "=" * 1000 + "\n")
+                        break
+        except Exception:
+            pass
+
+
+# поиск по всей указанной информации о пользователе
+def search_inf_users_vk_id(owner_id, abs_dir_path_user, lst_dict=config.lst_dict, token=config.token):
+    global path_file_inf_users_global
+    global count_key_data_global
+    url = "https://api.vk.com/method/execute?"
+    api = 'API.users.get({"user_ids":"' + str(owner_id) + '", "fields": "about, activities, books, games, interests, movies, music, nickname, quotes, status, tv"})'
+    code = f'return [{api}];'
+    data = dict(code=code, access_token=token, v='5.131')
+    resp = requests.post(url=url, data=data)
+    resp = resp.json()
+    data = {
+        "О себе": resp.get("response")[0][0].get("about", False),
+        "Деятельность": resp.get("response")[0][0].get("activities", False),
+        "Любимые книги": resp.get("response")[0][0].get("books", False),
+        "Любимые игры": resp.get("response")[0][0].get("games", False),
+        "Интересы": resp.get("response")[0][0].get("interests", False),
+        "Любимые фильмы": resp.get("response")[0][0].get("movies", False),
+        "Любимая музыка": resp.get("response")[0][0].get("music", False),
+        "Никнейм ": resp.get("response")[0][0].get("nickname", False),
+        "Любимые цитаты": resp.get("response")[0][0].get("quotes", False),
+        "Статус пользователя": resp.get("response")[0][0].get("status", False),
+        "Любимые телешоу": resp.get("response")[0][0].get("tv", False)
+    }
+
+    for key, value in list(data.items()):
+        if not value:
+            del data[key]
+
+    if not data:
+        path_file_inf_users_global = ""
+        count_key_data_global  = 0
+        return
+    else:
+        path_file_inf_users_global = os.path.join(abs_dir_path_user, f"inf_user_{owner_id}.txt")
+        count_key_data_global = len(data.keys())
+        try:
+            for key, value in data.items():
+                for word in lst_dict:
+                    if word.lower() in value.lower():
+                        with open(os.path.join(abs_dir_path_user, f"inf_user_{owner_id}.txt"), "a",
+                                  encoding="utf-8") as file:
+                            file.write(key + ": " + value + "\n" + "=" * 1000 + "\n")
+
+        except Exception:
+            pass
+# ====================================================================ОБОБЩАЮЩАЯ ФУНКЦИЯ================================
 # пробег по всем пользователям вк c использованием поиска фото, постов
 def collector(list_token, dir_path_date_and_time, dt):
     nest_asyncio.apply()
     data_photos = {}
-    data_text = {}
+    data_posts = {}
+    data_groups = {}
+    data_videos = {}
+    data_inf_users = {}
+
     async def bound_fetch_zero(sem, id, session):
         async with sem:
             await fetch_zero(id, session)
@@ -328,14 +500,15 @@ def collector(list_token, dir_path_date_and_time, dt):
 
                 # Проверяем если город=1(Москва) тогда добавляем в лист
                 for it in list_users:
+                    list_data.append(it[0]['id'])
                     try:
                         # если город Москва
-                        #if it[0]['city']['id'] == 1 and it[0]['id'] == 18:
+                        # if it[0]['city']['id'] == 1 and it[0]['id'] == 18:
                         if it[0]['id'] == 23:
-                            #ДОБАВЛЕНИЕ В СПИСОК ID СТРАНИЦЫ (ЗДЕСЬ НАЧИНАЕМ ПРОВЕРЯТЬ ФОТО)
+                            # ДОБАВЛЕНИЕ В СПИСОК ID СТРАНИЦЫ (ЗДЕСЬ НАЧИНАЕМ ПРОВЕРЯТЬ ИНФОРМАЦИЮ) TODO
 
                             # выкачивание фото
-                            downloading_search_photos(str(it[0]['id']), dir_path_date_and_time)
+                            # downloading_search_photos(str(it[0]['id']), dir_path_date_and_time)
 
                             dir_id_path = os.path.join(dir_path_date_and_time, str(it[0]['id']))
                             if not os.path.exists(dir_id_path):
@@ -343,8 +516,20 @@ def collector(list_token, dir_path_date_and_time, dt):
 
                             # выкачивание и проверка постов
                             search_post_vk_id(it[0]['id'], dir_id_path)
+
+                            # выкачивание и проверка названий групп и сообществ
+                            search_name_groups_vk_id(it[0]['id'], dir_id_path)
+
+                            # выкачивание и проверка названий видеозаписей
+                            search_name_videos_vk_id(it[0]['id'], dir_id_path)
+
+                            # выкачивание и провекра всей информации о пользователе
+                            search_inf_users_vk_id(it[0]['id'], dir_id_path)
+
+                            # если каталог пуст, удаляем
                             if not os.listdir(dir_id_path):
                                 os.rmdir(dir_id_path)
+
 
 
                             # инфомация для заполненеия базы
@@ -355,7 +540,11 @@ def collector(list_token, dir_path_date_and_time, dt):
                             category = "Раскрытие принадлежности к центральному аппарату МО РФ"
 
                             create_dict_data(url, check_date, count_record, path_dir_photos, category, data_photos)
-                            create_dict_data_post(url, check_date, count_text_global, path_file_text_global, "Новая категория", data_text)
+                            create_dict_data_post(url, check_date, count_text_global, path_file_text_global, "Новая категория (ПОСТЫ)", data_posts)
+                            create_dict_data_name_grops(url, check_date, count_text_groups_global, path_file_text_groups_global, "Новая категория (ГРУППЫ)", data_groups)
+                            create_dict_data_name_videos(url, check_date, count_video_global, path_file_video_global, "Категория ВИДЕО", data_videos)
+                            create_dict_data_inf_users(url, check_date, count_key_data_global, path_file_inf_users_global, "Категория ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЯХ", data_inf_users)
+
 
                     except Exception:
                         pass
@@ -371,13 +560,14 @@ def collector(list_token, dir_path_date_and_time, dt):
         url = 'https://api.vk.com/method/execute?access_token={}&v=5.101&code=return%20[{}];'.format(
             list_token[id % len(list_token)], api)
         return url
+
     async def run_zero(id):
         tasks = []
         sem = asyncio.Semaphore(1000)
 
         async with ClientSession() as session:
             #  Значение 3200 зависит от вашего числа токенов
-            param3200 = 1
+            param3200 = 250000
             for id in range((id - 1) * param3200, id * param3200):
                 task = asyncio.ensure_future(bound_fetch_zero(sem, id, session))
                 tasks.append(task)
@@ -386,15 +576,26 @@ def collector(list_token, dir_path_date_and_time, dt):
             await responses
             del responses
             await session.close()
+
+
     # Запускаем  сборщик
     # threshold = 17
-    threshold = 1
-    for i in range(0, threshold):
-        param_count = 1
-        for id in range(i * param_count + 1, (i + 1) * param_count + 1):
-            # print(id)
+    # param_count = 554
+    st = time()
+    threshold = 3
+    for i in range(threshold):
+        param_count = 40
+        for query in range(i * param_count + 1, (i + 1) * param_count + 1):
+            print(query, time()-st)
+            st = time()
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            loop.run_until_complete(run_zero(id))
+            loop.run_until_complete(run_zero(query))
 
-    return data_photos, data_text
+    print(len(list_data))
+
+    return {"photos":data_photos,
+            "posts":data_posts,
+            "groups":data_groups,
+            "videos":data_videos,
+            "inf_user":data_inf_users}
--- genering_profils_vk/src/main.py
+++ genering_profils_vk/src/main.py
@@ -2,7 +2,7 @@ from genering_profils_vk.src.func import *
 from pprint import pprint
 
 list_token = []
-with open(os.path.join("..", "tokens", "tokens.txt"), 'r') as f:
+with open(os.path.join("..", "tokens", "list_tokens_new.txt"), 'r') as f:
     for line in f:
         list_token.append(str(line).rstrip('\n'))
 
@@ -14,8 +14,14 @@ dir_path_date_and_time = create_dir_current_date_and_time(dt)
 start = time()
 data_id = collector(list_token, dir_path_date_and_time, dt)
 print("Итоговое время", time() - start)
-pprint(data_id[0])
+pprint(data_id["photos"])
 print()
-pprint(data_id[1])
+pprint(data_id["posts"])
+print()
+pprint(data_id["groups"])
+print()
+pprint(data_id["videos"])
+print()
+pprint(data_id["inf_user"])
 
 
