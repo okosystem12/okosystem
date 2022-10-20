7ca6bcbc508c6962a5d0985d2e7f5135cb36d5ad
okosystem12 <okosystem12@gmail.com>
2022-10-20 11:18:14 +0300

    d12

--- Site/static/Site/js/pages/control/view/panel/search/form/submit.js
+++ Site/static/Site/js/pages/control/view/panel/search/form/submit.js
@@ -3,6 +3,7 @@ import {add} from "../../../../../../req/control/social/add";
 export const submit = (form, e) => {
     e.preventDefault();
     const value = $(form).find('input[name=socialValue]').val();
+    console.log(value);
     add({value}, (msg) => {
             console.log(msg)
     });
