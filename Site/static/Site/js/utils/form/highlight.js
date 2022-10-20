774df6fea8b1c298267d938aea5a5215606cdb57
okosystem12 <okosystem12@gmail.com>
2022-10-20 16:33:24 +0300

    d12

--- Site/static/Site/js/utils/form/highlight.js
+++ Site/static/Site/js/utils/form/highlight.js
@@ -1,13 +1,15 @@
 export const highlight = (element, text = '') => {
     const _e = $(element);
     const parent = _e.parents(".form-group");
-    parent.addClass("has-error").addClass("has-feedback");
+
+    $(parent).addClass("has-error").addClass("has-feedback");
 
     if (!_e.is('select')) {
        _e.next("span").addClass("glyphicon-remove");
     }
 
     if (text) {
-        parent.find(".help-block").html(text);
+        $(parent).find(".help-block").html(text);
+        $(parent).click();
     }
 };
--- genering_profils_vk/src/tmp.py
+++ genering_profils_vk/src/tmp.py
@@ -13,13 +13,16 @@ from Site.models import AllUsersVK
 
 
 def read_files_for_bd(path_dir):
+
+    AllUsersVK.objects.all().delete()
+    return
+
     for namefile in os.listdir(path_dir):
         with open(os.path.join(path_dir, namefile), "r", encoding="utf-8") as file:
             data = [json.loads(line) for line in file]
 
         data_update = [elem for l in data for elem in l if
                        elem.get("deactivated") not in ("deleted", "banned") and not elem.get("is_closed")]
-
         for l in data_update:
             bdate = l.get("bdate", "")
             city = l.get("city", "")
@@ -38,5 +41,4 @@ def read_files_for_bd(path_dir):
         data.clear()
         data_update.clear()
 
-
-# read_files_for_bd(os.path.abspath(os.path.join("..", "database")))
+read_files_for_bd(os.path.abspath(os.path.join("..", "database")))
