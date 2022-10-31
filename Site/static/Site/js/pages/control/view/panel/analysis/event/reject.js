267859ca310987fba5835c32d629daa54c079cd0
okosystem12 <okosystem12@gmail.com>
2022-10-31 17:27:32 +0300

    d15

--- Site/static/Site/js/pages/control/view/panel/analysis/event/reject.js
+++ Site/static/Site/js/pages/control/view/panel/analysis/event/reject.js
@@ -1,12 +1,8 @@
 import {confirmModal} from "../../../../../../utils/confirmModal";
-import {componentsData} from "../../../../componentsData";
 import {reject as setReject} from "../../../../../../req/control/analysis/reject";
 import {response} from "./response";
 
 export const reject = (e) => {
-    confirmModal('Материалы не содержат указанного нарушения?', ()=>{
-        // const {viewSearch} = componentsData;
-        // setReject(e.currentTarget.dataset.id, response);
-        // viewSearch.find(`.social-list[data-social="${e.currentTarget.dataset.social}"]`).remove();
-    });
+    confirmModal('Материалы не содержат указанного нарушения?', () =>
+        setReject(e.currentTarget.dataset.id, response));
 };
